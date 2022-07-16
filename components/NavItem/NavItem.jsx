fe5dc23c4b44bc971c18f17cf6141955aec5ca0b
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2022-07-16 08:18:19 +0530

    animation using clip path

--- components/NavItem/NavItem.jsx
+++ components/NavItem/NavItem.jsx
@@ -11,15 +11,20 @@ export const NavItem = ({ children }) => {
       {children}{" "}
       {isOpen && (
         <motion.div
+        initial={{opacity: 0, x: 2, y: -2}}
           animate={{
-            clipPath: `circle(${800 * 2 + 200}px at 40px 40px)`,
+            clipPath: `circle(${1000 * 2 + 200}px at 40px 40px)`,
             transition: {
               type: "spring",
               stiffness: 20,
-              restDelta: 2
-            }
+              restDelta: 2,
+              delayChildren: 0.5
+            },
+            opacity: 1,
+            x: 0,
+            y: 0
           }}
-          transition={{ duration: 0.2 }}
+          transition={{ duration: 5 }}
           className="w-40 absolute  bg-gray-100 shadow-xl p-4 rounded flex flex-col items-center space-y-4"
         >
           <motion.div>Product 1</motion.div>
