9a16520a7597c75c27e1d38495a8dfb8d64586a3
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2022-07-16 07:18:53 +0530

    add tw and motion

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import Script from "next/script";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src="https://cdn.tailwindcss.com"></Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
