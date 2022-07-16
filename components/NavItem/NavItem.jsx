1f6532bcf5f9677a7dc35637491bd12ec18a8204
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2022-07-16 07:58:14 +0530

    add animation

--- components/NavItem/NavItem.jsx
+++ components/NavItem/NavItem.jsx
@@ -1,20 +1,35 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 export const NavItem = ({ children }) => {
-    const [isOpen, setIsOpen] = useState(true)
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div  onClick={() => setIsOpen(!isOpen)} className=" cursor-pointer relative"> 
+    <motion.div
+      onClick={() => setIsOpen(!isOpen)}
+      className=" cursor-pointer relative"
+    >
       {children}{" "}
       {isOpen && (
-        <div className="w-40 absolute right-px left-px bg-gray-100 shadow-xl p-4 rounded flex flex-col items-center space-y-4">
-          <div >Product 1</div>
-          <div >Product 1</div>
-          <div >Product 1</div>
-          <div >Product 1</div>
-          <div >Product 1</div>
-          <div >Product 1</div>
-        </div>
+        <motion.div
+          animate={{
+            clipPath: `circle(${800 * 2 + 200}px at 40px 40px)`,
+            transition: {
+              type: "spring",
+              stiffness: 20,
+              restDelta: 2
+            }
+          }}
+          transition={{ duration: 0.2 }}
+          className="w-40 absolute  bg-gray-100 shadow-xl p-4 rounded flex flex-col items-center space-y-4"
+        >
+          <motion.div>Product 1</motion.div>
+          <motion.div>Product 1</motion.div>
+          <motion.div>Product 1</motion.div>
+          <motion.div>Product 1</motion.div>
+          <motion.div>Product 1</motion.div>
+          <motion.div>Product 1</motion.div>
+        </motion.div>
       )}
-    </div>
+    </motion.div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
 import { NavItem } from "../components";
+import { motion } from "framer-motion";
 export default function Home() {
   return (
-    <div className="bg-gray-200 min-h-screen">
-      <div className="h-20 bg-teal-400 flex items-center justify-between py-4 px-20">
-        <div>Logo</div>
-        <div className="flex space-x-4">
-          <div>about</div>
-          <div>details</div>
+    <motion.div className="bg-gray-200 min-h-screen">
+      <motion.div className="h-20 bg-teal-400 flex items-center justify-between py-4 px-20">
+        <motion.div>Logo</motion.div>
+        <motion.div className="flex space-x-4">
+          <motion.div>about</motion.div>
+          <motion.div>details</motion.div>
           <NavItem>more</NavItem>
-          <div>contact</div>
-        </div>
-      </div>
-    </div>
+          <motion.div>contact</motion.div>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 }
