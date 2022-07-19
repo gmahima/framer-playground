fd5e2d9c8f73c0e0b77d582e5b3b886fb36361d0
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2022-07-19 13:17:46 +0530

    use prop

--- components/LandingPara/LandingPara.jsx
+++ components/LandingPara/LandingPara.jsx
@@ -1,20 +1,38 @@
-
 import { LandingParaLayout } from "./LandingPara.styles";
 import { useInView, useAnimationControls } from "framer-motion";
 import { useRef, useEffect } from "react";
-export const LandingPara = ({children, dir}) => {
-    let offset = dir == "left" ? -20: 20;
-    const controls = useAnimationControls()
-    const ref = useRef(null)
-    const isInView = useInView(ref)
-    useEffect(() => {
-        
-        if(isInView) {
-            console.log("Element is in view: ", isInView)
-            controls.start({opacity: 1, x: 0, transition: {delay: 0.3, duration: 0.3} })
-        }
-      }, [isInView])
-    return (
-        <LandingParaLayout.Container initial={{opacity: 0, x: offset}} dir={dir} ref={ref} animate={controls}>{children}</LandingParaLayout.Container>
-    )
-}
+export const LandingPara = ({ children, dir }) => {
+  let offset = dir == "left" ? -20 : 20;
+//   const controls = useAnimationControls();
+//   const ref = useRef(null);
+//   const isInView = useInView(ref);
+//   useEffect(() => {
+//     if (isInView) {
+//       console.log("Element is in view: ", isInView);
+//       controls.start({
+//         opacity: 1,
+//         x: 0,
+//         transition: { delay: 0.3, duration: 0.3 },
+//       });
+//     } else {
+//       controls.start({
+//         opacity: 0,
+//         x: offset,
+//         transition: { delay: 0.3, duration: 0.3 },
+//       });
+//     }
+//   }, [isInView]);
+  return (
+    <LandingParaLayout.Container
+      initial={{ opacity: 0, x: offset }}
+      dir={dir}
+      whileInView={{
+        opacity: 1,
+        x: 0,
+        transition: { delay: 0.3, duration: 0.3 },
+      }}
+    >
+      {children}
+    </LandingParaLayout.Container>
+  );
+};
