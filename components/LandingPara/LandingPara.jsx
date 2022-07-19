e948d88e730b70cae734d67a0650c55253739776
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2022-07-19 17:41:51 +0530

    add variants

--- components/LandingPara/LandingPara.jsx
+++ components/LandingPara/LandingPara.jsx
@@ -1,23 +1,23 @@
 import { LandingParaLayout } from "./LandingPara.styles";
 import { useInView, useAnimationControls } from "framer-motion";
 import { useRef, useEffect } from "react";
+import { animationVariants } from "./landingParaVariants";
 export const LandingPara = ({ children, dir }) => {
   let offset = dir == "left" ? -20 : 20;
   const controls = useAnimationControls();
   const ref = useRef(null);
-  const isInView = useInView(ref, {once: true});
+  const isInView = useInView(ref, { once: true });
   useEffect(() => {
     if (isInView) {
-      controls.start({
-        opacity: 1,
-        x: 0,
-        transition: { delay: 0.3, duration: 0.3 },
-      });
+      controls.start(
+        animationVariants.animate
+      );
     }
   }, [isInView]);
   return (
     <LandingParaLayout.Container
-      initial={{ opacity: 0, x: offset }}
+      variants={animationVariants}
+      initial={dir == "left" ? "initialFromLeft" : "initialFromRight"}
       dir={dir}
       ref={ref}
       animate={controls}
---
+++ components/LandingPara/landingParaVariants.js
@@ -0,0 +1,9 @@
+export const animationVariants = {
+    animate: {
+        opacity: 1,
+        x: 0,
+        transition: { delay: 0.3, duration: 0.3 },
+      },
+      initialFromLeft: { opacity: 0, x: 10 },
+      initialFromRight: {opacity: 0, x: -10}
+}
