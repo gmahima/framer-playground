de3f00976f159e51c3ca8e678cb0c7318642d231
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2022-07-21 11:54:37 +0530

    add autoplay to tabslider and animation to text change

--- components/ChangingText/ChangingText.jsx
+++ components/ChangingText/ChangingText.jsx
@@ -1,24 +1,27 @@
 import { ChangingTextLayout } from "./ChangingText.styles";
 import { useState, useEffect, useRef } from "react";
-import { motion, useInView } from "framer-motion";
+import { AnimatePresence, motion, useInView } from "framer-motion";
 import { lorem } from "../../data";
 let arr = [
   { text: "hi", bg: "primary" },
   { text: "lorem", bg: "secondary" },
-  { text: "ipsum", bg: "theme.colors.bg" },
+  { text: "ipsum", bg: "bg" },
+  { text: "dolor", bg: "highlight" }
 ];
 export const ChangingText = ({ children }) => {
-  let [selectedItem, setSelectedItem] = useState(0);
+  let [selectedItem, setSelectedItem] = useState(arr[0]);
   const ref = useRef(null);
   const isInView = useInView(ref);
+
   useEffect(() => {
-    if (isInView) {
+    if (true) {
       const interval = setTimeout(() => {
+        let i = arr.findIndex(ob => ob == selectedItem)
         console.log("tick", selectedItem);
-        if (selectedItem == arr.length - 1) {
-          setSelectedItem(0);
+        if (selectedItem == arr[arr.length - 1]) {
+          setSelectedItem(arr[0]);
         } else {
-          setSelectedItem(selectedItem + 1);
+          setSelectedItem(arr[i+1]);
         }
       }, 1000 * 2);
 
@@ -26,14 +29,23 @@ export const ChangingText = ({ children }) => {
         clearInterval(interval);
       };
     }
-  }, [selectedItem, isInView]);
-
+  }, [selectedItem]);
   return (
     <div>
       <div>{lorem.long}</div>
-      <ChangingTextLayout.Container ref={ref} bg={arr[selectedItem].bg}>
-        {arr[selectedItem].text}
-      </ChangingTextLayout.Container>
+
+      <AnimatePresence>
+        <ChangingTextLayout.Container
+          ref={ref}
+          bg={selectedItem.bg}
+          initial={{ y: -10 }}
+          animate={{ y: 0 }}
+          exit={{ y: 10 }}
+          key={selectedItem? selectedItem.text : "x"}
+        >
+          {selectedItem.text}
+        </ChangingTextLayout.Container>
+      </AnimatePresence>
     </div>
   );
 };
--- components/TabSlider/TabSlider.jsx
+++ components/TabSlider/TabSlider.jsx
@@ -5,8 +5,26 @@ import { ingredients } from "../../data";
 import { useState } from "react";
 import { Tab } from "./Tab";
 import { TabBar } from "./TabBar";
+import { useEffect } from "react";
 export const TabSlider = ({ children }) => {
   const [selectedItem, setSelectedItem] = useState(ingredients[0]);
+  useEffect(() => {
+    if (true) {
+      const interval = setTimeout(() => {
+        let i = ingredients.findIndex(ob => ob == selectedItem)
+        console.log("tick", selectedItem);
+        if (selectedItem == ingredients[ingredients.length - 1]) {
+          setSelectedItem(ingredients[0]);
+        } else {
+          setSelectedItem(ingredients[i+1]);
+        }
+      }, 1000 * 2);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [selectedItem]);
   return (
     <TabSliderLayout.Container>
       <TabBar>
