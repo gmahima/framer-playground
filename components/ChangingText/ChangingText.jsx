e006b4beb85def10e6362e59d7b66d2a8fa8087e
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2022-07-21 18:12:08 +0530

    remove unused component and prettify

--- components/ChangingText/ChangingText.jsx
+++ components/ChangingText/ChangingText.jsx
@@ -1,69 +1,55 @@
 import { ChangingTextLayout } from "./ChangingText.styles";
-import { useRef } from "react";
 import { lorem } from "../../data";
 import { useTheme } from "styled-components";
 
-
 export const ChangingText = () => {
-  const ref = useRef(null);
   const theme = useTheme();
-  const times = [0, .33, .36, .63, .66, .97, 1];
-
- 
+  const times = [0, 0.33, 0.36, 0.63, 0.66, 0.97, 1];
 
   return (
-    <div>
+    <>
       <ChangingTextLayout.Container
-        ref={ref}
         animate={{
-            backgroundColor: [
-                theme.colors.primary, 
-                theme.colors.primary,
-                theme.colors.secondary,
-                theme.colors.secondary,  
-                theme.colors.highlight,
-                theme.colors.highlight,
-                theme.colors.primary
-                ],
-        }}        
+          backgroundColor: [
+            theme.colors.primary,
+            theme.colors.primary,
+            theme.colors.secondary,
+            theme.colors.secondary,
+            theme.colors.highlight,
+            theme.colors.highlight,
+            theme.colors.primary,
+          ],
+        }}
         transition={{
-            duration: 9,
-            repeat: Infinity,
-            repeatType: "loop",
-            type: "linear",
-            times
+          duration: 9,
+          repeat: Infinity,
+          repeatType: "loop",
+          type: "linear",
+          times,
         }}
       >
         <ChangingTextLayout.ChangingTextContainer>
-        <ChangingTextLayout.ChangingText    
-                animate={{
-                    y: [
-                        "0%",
-                        "0%",
-                        "-33%",
-                        "-33%",
-                        "-66%",
-                        "-66%",
-                        "0%"
-                    ],                   
-                }}
-             transition={{
-                duration: 9,
-                repeat: Infinity,
-                repeatType: "loop",
-                type: "linear",
-                times
-             }}                         
-            >
-              <p>Hi</p>
-              <p>Hello</p>
-              <p>How are you ?</p>
-            </ChangingTextLayout.ChangingText>
+          <ChangingTextLayout.ChangingText
+            animate={{
+              y: ["0%", "0%", "-33%", "-33%", "-66%", "-66%", "0%"],
+            }}
+            transition={{
+              duration: 9,
+              repeat: Infinity,
+              repeatType: "loop",
+              type: "linear",
+              times,
+            }}
+          >
+            <p>Hi</p>
+            <p>Hello</p>
+            <p>How are you ?</p>
+          </ChangingTextLayout.ChangingText>
         </ChangingTextLayout.ChangingTextContainer>
         <ChangingTextLayout.OtherContent>
           {lorem.short}
         </ChangingTextLayout.OtherContent>
       </ChangingTextLayout.Container>
-    </div>
+    </>
   );
 };
